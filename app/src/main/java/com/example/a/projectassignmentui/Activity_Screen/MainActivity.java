cb6edd3d38d08c511f78115ac3affdfa80882b91
thienphu103 <thienphu103@gmail.com>
2017-10-16 17:57:23 +0700

    update

--- app/src/main/java/com/example/a/projectassignmentui/Activity_Screen/MainActivity.java
+++ app/src/main/java/com/example/a/projectassignmentui/Activity_Screen/MainActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -13,11 +15,13 @@ import android.widget.RelativeLayout;
 import com.example.a.projectassignmentui.BaseActivity.SoftKeyboardCatch;
 import com.example.a.projectassignmentui.R;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
     SoftKeyboardCatch softKeyboard;
     EditText edtEmail;
     EditText edtPass;
     ImageView imageView;
+    Button btnResgiter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +29,22 @@ public class MainActivity extends AppCompatActivity{
 //        relativeLayout = (RelativeLayout) findViewById(R.id.layout_main_focus);
 //        relativeLayout.setVisibility(View.GONE);
         getSupportActionBar().hide();
-        edtEmail = (EditText) findViewById(R.id.editTextEmail);
-        edtPass = (EditText) findViewById(R.id.editTextPass);
-        imageView =(ImageView) findViewById(R.id.image_view1);
-        RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.layout_main); // You must use the layout root
-        InputMethodManager im = (InputMethodManager) getSystemService(Service.INPUT_METHOD_SERVICE);
-        softKeyboard = new SoftKeyboardCatch(mainLayout, im);
+        initControl();
+        intiEvent();
+        initDisplay();
+    }
+
+    private void initDisplay() {
+
+    }
+
+    private void intiEvent() {
+        btnResgiter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         softKeyboard.setSoftKeyboardCallback(new SoftKeyboardCatch.SoftKeyboardChanged() {
 
             @Override
@@ -40,7 +54,7 @@ public class MainActivity extends AppCompatActivity{
                 new Handler(Looper.getMainLooper()).post(new Runnable() {
                     @Override
                     public void run() {
-                        hideKeyboard();
+//                        hideKeyboard();
                     }
                 });
             }
@@ -52,18 +66,31 @@ public class MainActivity extends AppCompatActivity{
                 new Handler(Looper.getMainLooper()).post(new Runnable() {
                     @Override
                     public void run() {
-                        showKeyboard();
+//                        showKeyboard();
                     }
                 });
             }
         });
     }
-    private void showKeyboard(){
-        imageView.setImageResource(R.drawable.iconfb2);
+
+    private void initControl() {
+        edtEmail = (EditText) findViewById(R.id.editTextEmail);
+        edtPass = (EditText) findViewById(R.id.editTextPass);
+        imageView = (ImageView) findViewById(R.id.image_view1);
+        btnResgiter = (Button) findViewById(R.id.btn_resgiter);
+
+        RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.layout_main); // You must use the layout root
+        InputMethodManager im = (InputMethodManager) getSystemService(Service.INPUT_METHOD_SERVICE);
+        softKeyboard = new SoftKeyboardCatch(mainLayout, im);
     }
-    private void hideKeyboard(){
-        imageView.setImageResource(R.drawable.banner);
-    }
+
+//    private void showKeyboard() {
+//        imageView.setImageResource(R.drawable.iconfb2);
+//    }
+//
+//    private void hideKeyboard() {
+//        imageView.setImageResource(R.drawable.banner);
+//    }
 
 }
 //    public void showKeyboard(View v) {
